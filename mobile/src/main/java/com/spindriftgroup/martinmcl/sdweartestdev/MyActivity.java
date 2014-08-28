a788533e0b532c1d3ff90801a038e02ff93432d8
sathishkumar <sathishkumarpalanivelu@Sathishkumars-MacBook-Pro.local>
2014-08-28 11:35:13 +0100

    test

--- mobile/src/main/java/com/spindriftgroup/martinmcl/sdweartestdev/MyActivity.java
+++ mobile/src/main/java/com/spindriftgroup/martinmcl/sdweartestdev/MyActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MyActivity extends Activity {
-
+//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
